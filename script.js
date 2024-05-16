8b6aa0ebb97c5e3c95a2847143092b151401c810
Shuvaranjan <shuvaranjans@gmail.com>
2024-05-16 16:01:53 +0530

    UPDATED!

---
+++ script.js
@@ -0,0 +1,21 @@
+const sr = ScrollReveal ({
+   
+    distance: '40px',
+    duration: 2050,
+    delay: 200,
+    reset: true
+});
+
+sr.reveal('.content', {origin: 'top'})
+sr.reveal('.img-box img', {origin: 'bottom'})
+sr.reveal('.about-text', {origin: 'left'})
+sr.reveal('.about-text h2', {origin: 'top'})
+sr.reveal('.Services h2', {origin: 'top'})
+sr.reveal('.projects h2', {origin: 'top'})
+sr.reveal('.projects-box', {origin: 'bottom'})
+sr.reveal('.hire-me p', {origin: 'bottom'})
+sr.reveal('.hire-me .button2', {origin: 'bottom'})
+sr.reveal('.contact-text', {origin: 'left'})
+sr.reveal('.contact-form', {origin: 'right'})
+sr.reveal('.about img', {origin: 'bottom'})
+sr.reveal('.Services .box', {origin: 'bottom'})
